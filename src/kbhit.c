a3bfd9dcdfa77be05164c778d83b0d2ae1d8223f
qubitkiwi <asong1230@gmail.com>
2026-06-05 09:42:08 +0900

    STDIN_FILENO 명시

--- src/kbhit.c
+++ src/kbhit.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 #include <termios.h>
 #include <fcntl.h>
+#include <unistd.h>
 
 #include "kbhit.h"
 
 static struct termios oldt, newt;
-static int old_stdin_fd, new_stdin_fd;
+static int old_stdin_fd;
 
 
 int kbhit_init()
@@ -14,19 +15,19 @@ int kbhit_init()
 	tcgetattr(0, &oldt);
 	newt = oldt;
 	newt.c_lflag &= ~(ICANON | ECHO);
-	tcsetattr(0, TCSANOW, &newt);
+	tcsetattr(STDIN_FILENO, TCSANOW, &newt);
 
 	// config stdin fd 
-	old_stdin_fd = fcntl(0, F_GETFL, 0);
-	fcntl(0, F_SETFL, old_stdin_fd | O_NONBLOCK);
+	old_stdin_fd = fcntl(STDIN_FILENO, F_GETFL, 0);
+	fcntl(STDIN_FILENO, F_SETFL, old_stdin_fd | O_NONBLOCK);
 
 	return 0;
 }
 
 int kbhit_uninit()
 {
-	tcsetattr(0, TCSANOW, &oldt);
-	fcntl(0, F_SETFL, old_stdin_fd);
+	tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
+	fcntl(STDIN_FILENO, F_SETFL, old_stdin_fd);
 
 	return 0;
 }
